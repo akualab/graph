fac79b4f751cbde19aeaffd6ba8fa79b9d2a83f7
Leo Neumeyer <leoneu@apache.org>
2014-01-11 18:11:33 -0800

    Convert viterbi test to example.

--- example_viterbi_test.go
+++ example_viterbi_test.go
@@ -6,31 +6,37 @@
 package graph
 
 import (
+	"fmt"
 	"math"
-	"testing"
 )
 
+// Create a type to implement the Viterbier interface.
 type vvalue struct {
 	f ScoreFunc
 }
 
+// Implements the Viterbier interface.
 func (v vvalue) ScoreFunction(n int, node *Node) float64 {
 	return v.f(n, node)
 }
 
-func simpleGraph(t *testing.T) *Graph {
+// Create a simple graph.
+func simpleGraph() *Graph {
 
+	// Some sequence of probabilities. (rows correspond to states.)
 	obs := [][]float64{
 		{0.1, 0.1, 0.2, 0.4, 0.11, 0.11, 0.12, 0.14},
 		{0.4, 0.1, 0.3, 0.5, 0.21, 0.01, 0.12, 0.08},
 		{0.2, 0.2, 0.4, 0.5, 0.09, 0.11, 0.32, 0.444},
 	}
+
+	// Convert to log probabilities.
 	for i, v := range obs {
 		for j, _ := range v {
 			obs[i][j] = math.Log(obs[i][j])
 		}
 	}
-	// Define score functions to implement edit distance.
+	// Define score functions to return state probabilities.
 	var s1Func = func(n int, node *Node) float64 {
 		return obs[0][n]
 	}
@@ -47,9 +53,10 @@ func simpleGraph(t *testing.T) *Graph {
 		return 0
 	}
 
+	// Creates a new graph.
 	g := New()
 
-	// set some nodes
+	// Create some nodes and assign values.
 	g.Set("s0", vvalue{}) // initial state
 	g.Set("s1", vvalue{s1Func})
 	g.Set("s2", vvalue{s2Func})
@@ -57,7 +64,7 @@ func simpleGraph(t *testing.T) *Graph {
 	g.Set("s5", vvalue{s5Func})
 	g.Set("s4", vvalue{finalFunc}) // final state
 
-	// make some connections
+	// Make connections.
 	g.Connect("s0", "s1", 1)
 	g.Connect("s1", "s1", 0.4)
 	g.Connect("s1", "s2", 0.5)
@@ -70,31 +77,41 @@ func simpleGraph(t *testing.T) *Graph {
 	g.Connect("s3", "s3", 0.6)
 	g.Connect("s3", "s4", 0.4)
 
+	// Convert transition probabilities to log.
 	g.ConvertToLogProbs()
 	return g
 }
 
-func TestViterbi(t *testing.T) {
+// This example shows how to run a Viterbi decoder on a simple graph.
+func ExampleDecoder() {
 
 	var start, end *Node
 	var e error
 
-	g := simpleGraph(t)
-	t.Logf("three state graph:\n%s\n", g)
+	// Create the graph.
+	g := simpleGraph()
+	e = fmt.Errorf("simple graph:\n%s\n", g)
+	panic(e)
 
+	// Define the start and end nodes.
 	if start, e = g.Get("s0"); e != nil {
-		t.Fatal(e)
+		panic(e)
 	}
 	if end, e = g.Get("s4"); e != nil {
-		t.Fatal(e)
+		panic(e)
 	}
 
+	// Create a decoder.
 	dec, e := NewDecoder(g, start, end)
 	if e != nil {
-		t.Fatal(e)
+		panic(e)
 	}
+
+	// Find the optimnal sequence.
 	token := dec.Decode(8)
 
-	t.Logf("\n\n>>>> FINAL: %s\n", token)
-
+	// The token has the backtrace to find the optimal path.
+	fmt.Printf("\n\n>>>> FINAL: %s\n", token)
 }
+
+// Output:
